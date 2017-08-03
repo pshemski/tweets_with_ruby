bb7915e9f726179547deda4f040f7ba41dac232e
Przemek Markowski <p.markowski1980@gmail.com>
2017-08-03 17:13:42 +0100

    Initial commit

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,14 @@
+class TwitterApi
+	def self.my_tweets
+		client.user_timeline('PrzemekMarkows1')
+	end
+
+	def self.client 
+		@client ||= Twitter::REST::Client.new do |config|
+		  config.consumer_key        = ENV["CONSUMER_KEY"]
+		  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+		  config.access_token        = ENV["ACCESS_TOKEN"]
+		  config.access_token_secret = ENV["ACCESS_SECRET"]
+		end
+	end
+end
